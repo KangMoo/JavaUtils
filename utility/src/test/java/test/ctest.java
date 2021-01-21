45bb57f9fb8954f1c2658e337549dea21755b8e9
KangMoo <hkm0629@naver.com>
2021-01-21 17:04:04 +0900

    rebase test1

--- utility/src/test/java/test/ctest.java
+++ utility/src/test/java/test/ctest.java
@@ -1,22 +1,13 @@
 package test;
 
-import javafx.util.Pair;
 import org.junit.Test;
-import utility.TimeUtil;
-
-import java.text.ParseException;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * @author kangmoo Heo
  */
 public class ctest {
     @Test
-    public void test() throws ParseException {
-        String nowTime =TimeUtil.dateFormat(System.currentTimeMillis(),"yyyHHmm");
-        System.out.println("nowTime = " + nowTime);
-        System.out.println(TimeUtil.dateFormat(nowTime,"yyyyHHmm"));
+    public void test() {
+        // Git Rebase Test1
     }
+
 }
