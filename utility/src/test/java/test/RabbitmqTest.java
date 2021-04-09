a99713bb62e1c5df3af7ae05391e3aa909d53036
hkm0629 <hkm0629@uangel.com>
2021-04-09 15:54:52 +0900

    Update

--- utility/src/test/java/test/RabbitmqTest.java
+++ utility/src/test/java/test/RabbitmqTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import rqbbitmq.*;
 
 import java.util.Date;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -12,36 +13,33 @@ import java.util.Date;
  */
 public class RabbitmqTest {
 
-    private String host = "127.0.0.1";       // Put Rmq Info
-    private String userName = "userName";    // Put Rmq Info
-    private String password = "password";    // Put Rmq Info
-    private String queueName = "queueName";  // Put Rmq Info
-
-    private RmqReceiver rmqReceiver;
-    private RmqSender rmqSender;
+    // Put Rabbitmq Info
+    private String host = "host";
+    private String userName = "userName";
+    private String password = "password";
+    private String queueName = "queueName";
 
     @Test
     public void rabbitmqTest() {
-        startRmqServer();
-        startRmqClient();
+        RmqReceiver rmqReceiver = startRmqServer();
+        RmqSender rmqSender = startRmqClient();
 
         // Message Send
-        for (int i = 0; i < 10; i++) {
-            String msg = "Test Msg_" + i;
-            byte[] msgByte = msg.getBytes();
-            rmqSender.send(msgByte, msgByte.length);
-        }
+        IntStream.range(0, 10).mapToObj(i -> ("MSG_" + i).getBytes()).forEach(msg -> rmqSender.send(msg, msg.length));
 
-        // Waiting to receive a message(1 sec)
+        // Waiting to receive message(1 sec)
         try {
             Thread.sleep(1000);
-        } catch (Exception e){
+        } catch (Exception e) {
             // Do nothing
         }
+
+        rmqReceiver.close();
+        rmqSender.close();
     }
 
-    private void startRmqServer() {
-        rmqReceiver = new RmqReceiver(host, userName, password, queueName, new RmqCallback() {
+    private RmqReceiver startRmqServer() {
+        RmqReceiver rmqReceiver = new RmqReceiver(host, userName, password, queueName, new RmqCallback() {
             @Override
             public void onReceived(String msg, Date ts) {
                 System.out.println("Recv msg = " + msg);
@@ -54,10 +52,11 @@ public class RabbitmqTest {
         });
         rmqReceiver.connectServer();
         rmqReceiver.start();
+        return rmqReceiver;
     }
 
-    private void startRmqClient() {
-        rmqSender = new RmqSender(host, userName, password, queueName, new RmqCallback() {
+    private RmqSender startRmqClient() {
+        RmqSender rmqSender = new RmqSender(host, userName, password, queueName, new RmqCallback() {
             @Override
             public void onReceived(String msg, Date ts) {
                 // Do nothing
@@ -69,6 +68,7 @@ public class RabbitmqTest {
             }
         });
         rmqSender.connectClient();
+        return rmqSender;
     }
 
 }
