842c3aa8509daf984bf5f58d9634181a136a1a2f
hkm0629 <hkm0629@uangel.com>
2021-05-20 16:30:08 +0900

    Update

--- utility/src/test/java/test/UdpTest.java
+++ utility/src/test/java/test/UdpTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import udp.UdpClient;
 import udp.UdpServer;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 
@@ -14,20 +12,28 @@ import java.nio.charset.StandardCharsets;
  * @author kangmoo Heo
  */
 public class UdpTest {
+    String sendMsg = "Hello";
+    String recvMsg = null;
     @Test
     public void udpTest() throws SocketException {
-        UdpServer udpServer = new UdpServer(7000, (packet) -> System.out.println("packet 수신. " + new String(packet, StandardCharsets.UTF_8)));
+        UdpServer udpServer = new UdpServer(7000, (packet) -> {
+            String inputMsg = new String(packet, StandardCharsets.UTF_8);
+            System.out.println("packet Recv. " + inputMsg);
+            this.recvMsg = inputMsg;
+        });
         udpServer.start();
         UdpClient udpClient = new UdpClient(7001, "127.0.0.1", 7000);
+
         try {
             udpClient.connect();
-            udpClient.simpleSend("Hey!");
+            udpClient.simpleSend(sendMsg);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(100);
+            assert !sendMsg.equals(recvMsg);
         } catch (Exception e){
             e.printStackTrace();
         }
