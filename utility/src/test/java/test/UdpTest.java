6deaeb9e8c98290aa06d4ccb3bf15d11badcab5d
hkm0629 <hkm0629@uangel.com>
2021-05-24 08:32:34 +0900

    update

--- utility/src/test/java/test/UdpTest.java
+++ utility/src/test/java/test/UdpTest.java
@@ -2,8 +2,10 @@ package test;
 
 import org.junit.Test;
 import udp.UdpClient;
+import udp.UdpConn;
 import udp.UdpServer;
 
+import java.io.IOException;
 import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 
@@ -30,6 +32,27 @@ public class UdpTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        try {
+            Thread.sleep(100);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void udpTest2() throws IOException {
+        UdpConn udpConn1 = new UdpConn(7777, (packet) -> {
+            String inputMsg = new String(packet, StandardCharsets.UTF_8);
+            System.out.println("[7777] packet Recv. " + inputMsg);
+        });
+        UdpConn udpConn2 = new UdpConn(8888, (packet) -> {
+            String inputMsg = new String(packet, StandardCharsets.UTF_8);
+            System.out.println("[8888] packet Recv. " + inputMsg);
+        });
+
+        udpConn1.sendData("Hello", "127.0.0.1", 8888);
+        udpConn2.sendData("World", "127.0.0.1", 7777);
 
         try {
             Thread.sleep(100);
