a4ced3d2313f1ad29c2fa3957afd9c9e99aa9b61
hkm0629 <hkm0629@uangel.com>
2021-06-25 14:47:37 +0900

    update

--- utility/src/main/java/utility/CommandExecutor.java
+++ utility/src/main/java/utility/CommandExecutor.java
@@ -21,21 +21,17 @@ public class CommandExecutor {
         }
     }
 
-    public static String runCommandForOutput(String... commands) {
+    public static String runCommandForOutput(String... commands) throws Exception {
         ProcessBuilder pb = new ProcessBuilder(commands);
         Process p;
         String result = "";
-        try {
-            p = pb.start();
-            final BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
-            reader.lines().iterator().forEachRemaining(sj::add);
-            result = sj.toString();
-            p.waitFor();
-            p.destroy();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        p = pb.start();
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
+        reader.lines().iterator().forEachRemaining(sj::add);
+        result = sj.toString();
+        p.waitFor();
+        p.destroy();
         return result;
     }
 }
