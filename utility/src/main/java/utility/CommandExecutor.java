d7ec98d8e18c3d99fba7efd56466d1c9ed1f1f2d
hkm0629 <hkm0629@uangel.com>
2021-06-25 15:03:15 +0900

    update

--- utility/src/main/java/utility/CommandExecutor.java
+++ utility/src/main/java/utility/CommandExecutor.java
@@ -34,4 +34,12 @@ public class CommandExecutor {
         p.destroy();
         return result;
     }
+
+    public static String runBashCommandForOut(String... commands) throws Exception {
+        String[] newCommands = new String[commands.length+2];
+        newCommands[0] = "bash";
+        newCommands[1] = "-c";
+        System.arraycopy(commands, 0, newCommands, 2, commands.length);
+        return runCommandForOutput(newCommands);
+    }
 }
