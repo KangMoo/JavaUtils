916cd1b8f2fbeb737c0f783287be1bf474adea64
hkm0629 <hkm0629@uangel.com>
2021-06-25 15:19:58 +0900

    update

---
+++ utility/src/main/java/utility/Executor.java
@@ -0,0 +1,30 @@
+package utility;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ *
+ * @author kangmoo Heo
+ */
+public class Executor {
+    public static Future<?> execute(Runnable runnable){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            return executorService.submit(runnable);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    public static <T> Future<T> execute(Callable<T> task){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            return executorService.submit(task);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+}
