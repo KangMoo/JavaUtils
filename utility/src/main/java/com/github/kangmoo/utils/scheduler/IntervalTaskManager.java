a0e4a62b7772aba4d3c13ccf721498149d715f9d
hkm0629 <hkm0629@uangel.com>
2023-04-09 22:42:19 +0900

    update

--- utility/src/main/java/com/github/kangmoo/utils/scheduler/IntervalTaskManager.java
+++ utility/src/main/java/com/github/kangmoo/utils/scheduler/IntervalTaskManager.java
@@ -1,95 +1,27 @@
 package com.github.kangmoo.utils.scheduler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.List;
 
 /**
- * Session Manager
- * 주기적으로 세션을 필터링하고 처리 (Default 1초)
- *
  * @author Kangmoo Heo
  */
+@Slf4j
 public class IntervalTaskManager {
-    private static final Logger logger = LoggerFactory.getLogger(IntervalTaskManager.class);
-    private static final IntervalTaskManager instance = new IntervalTaskManager();
-    private final Map<String, IntervalTaskUnit> jobs = new HashMap<>();
-    private Set<ScheduledExecutorService> executorService;
-    private int defaultInterval = 1000;
-    private boolean isStarted = false;
+
+    private static final List<IntervalTaskUnit> itu = List.of(
+            // new SessionMonitor()
+    );
 
     private IntervalTaskManager() {
     }
 
-    public static IntervalTaskManager getInstance() {
-        return instance;
+    public static void startAll() {
+        itu.forEach(IntervalTaskUnit::start);
     }
 
-    public IntervalTaskManager init() {
-        return this;
-    }
-
-    public void start() {
-        if (isStarted) {
-            logger.info("Already Started Interval Task Manager");
-            return;
-        }
-        isStarted = true;
-        executorService = new HashSet<>(jobs.size());
-        for (IntervalTaskUnit runner : jobs.values()) {
-            ScheduledExecutorService job = Executors.newSingleThreadScheduledExecutor(r -> new Thread(r, "ITV_" + runner.getClass().getSimpleName()));
-            executorService.add(job);
-            job.scheduleAtFixedRate(runner, runner.getInterval() - System.currentTimeMillis() % runner.getInterval(),
-                    runner.getInterval(), runner.timeUnit);
-        }
-        logger.info("Interval Task Manager Start");
-    }
-
-    public void stop() {
-        if (!isStarted) {
-            logger.info("Already Stopped Interval Task Manager");
-            return;
-        }
-        isStarted = false;
-        executorService.forEach(ExecutorService::shutdown);
-        logger.info("Interval Task Manager Stop");
-    }
-
-    public void addJob(String name, IntervalTaskUnit runner) {
-        if (jobs.get(name) != null) {
-            logger.warn("() () () Hashmap Key duplication");
-            return;
-        }
-        logger.debug("() () () Add Runner [{}]", name);
-        jobs.put(name, runner);
-    }
-
-    public void removeJob(String name) {
-        jobs.remove(name);
-    }
-
-    public void removeAllJob() {
-        jobs.clear();
-    }
-
-    public void sessionCheck() {
-        for (IntervalTaskUnit runner : jobs.values()) {
-            runner.run();
-        }
-    }
-
-    public int getDefaultInterval() {
-        return defaultInterval;
-    }
-
-    public void setDefaultInterval(int msec) {
-        this.defaultInterval = msec;
+    public static void stopAll() {
+        itu.forEach(IntervalTaskUnit::stop);
     }
 }
--- utility/src/main/java/com/github/kangmoo/utils/scheduler/IntervalTaskUnit.java
+++ utility/src/main/java/com/github/kangmoo/utils/scheduler/IntervalTaskUnit.java
@@ -1,21 +1,26 @@
 package com.github.kangmoo.utils.scheduler;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author kangmoo Heo
  */
+@Slf4j
 public abstract class IntervalTaskUnit implements Runnable {
+    protected static final Set<String> RUNNING_SCHEDULERS = new ConcurrentSkipListSet<>();
+    protected String id = "ITV_" + this.getClass().getSimpleName();
+    protected ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(r -> new Thread(r, id));
     protected int interval;
-    protected TimeUnit timeUnit = TimeUnit.MILLISECONDS;
 
-    public IntervalTaskUnit(int interval) {
+    protected IntervalTaskUnit(int interval) {
         this.interval = interval;
-    }
-
-    public IntervalTaskUnit(int interval, TimeUnit timeUnit) {
-        this.interval = interval;
-        this.timeUnit = timeUnit;
     }
 
     public int getInterval() {
@@ -24,5 +29,35 @@ public abstract class IntervalTaskUnit implements Runnable {
 
     public void setInterval(int interval) {
         this.interval = interval;
+    }
+
+    public void start() {
+        if (!RUNNING_SCHEDULERS.add(id)) {
+            log.warn("Interval Task Job Already Started {}", id);
+            return;
+        }
+
+        scheduledExecutorService.scheduleAtFixedRate(this::proc,
+                this.interval - System.currentTimeMillis() % this.interval,
+                this.interval, TimeUnit.MILLISECONDS);
+        log.info("Interval Task Job Start {}", id);
+    }
+
+    public void stop() {
+        if (!RUNNING_SCHEDULERS.remove(id)) {
+            log.warn("Interval Task Job Already Stopped {}", id);
+            return;
+        }
+
+        scheduledExecutorService.shutdown();
+        log.info("Interval Task Job Stop {}", id);
+    }
+
+    private void proc() {
+        try {
+            run();
+        } catch (Exception e) {
+            log.warn("Err Occurs", e);
+        }
     }
 }
