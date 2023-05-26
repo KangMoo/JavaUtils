a6006a849cb9958c379074209a6c5aaf30395485
hkm0629 <hkm0629@uangel.com>
2023-05-26 16:58:06 +0900

    Media 관련 유틸리티 추가

---
+++ utility/media/src/main/java/com/github/kangmoo/utils/audio/pcm/LpcmResampler.java
@@ -0,0 +1,37 @@
+package com.github.kangmoo.utils.audio.pcm;
+
+/**
+ * @author kangmoo Heo
+ */
+public class LpcmResampler {
+    /**
+     * Resample from 8k to 16k
+     *
+     * @param data The input data
+     * @return The resampled data
+     */
+    public static byte[] resample8kTo16k(byte[] data) {
+        byte[] resampled = new byte[data.length * 2];
+        for (int i = 0; i < data.length; i += 2) {
+            resampled[i * 2] = data[i];
+            resampled[i * 2 + 1] = data[i + 1];
+            resampled[i * 2 + 2] = data[i];
+            resampled[i * 2 + 3] = data[i + 1];
+        }
+        return resampled;
+    }
+
+    /**
+     * Resample from 16k to 8k
+     *
+     * @param data The input data
+     * @return The resampled data
+     */
+    public static byte[] resample16kTo8k(byte[] data) {
+        byte[] resampled = new byte[data.length / 2];
+        for (int i = 0; i < resampled.length; i++) {
+            resampled[i] = i % 2 == 0 ? data[i * 2] : data[i * 2 + 1];
+        }
+        return resampled;
+    }
+}
---
+++ utility/media/src/main/java/com/github/kangmoo/utils/audio/pcm/WavFile.java
@@ -0,0 +1,90 @@
+package com.github.kangmoo.utils.audio.pcm;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * @author kangmoo Heo
+ */
+public class WavFile {
+    /**
+     * Make Wave Header Bytes
+     *
+     * @param sampleRate  Sample rate - 8000, 16000, etc.
+     * @param channel Number of channels - Mono = 1, Stereo = 2, etc..
+     * @param format Number of bits per sample (16 here)
+     * @param dataSize Raw Data size
+     */
+    private static final int DEFAULT_BUFFER_SIZE = 8192;
+
+    public static byte[] createWavHeader(int sampleRate, int channel, int format, int dataSize) {
+        byte[] header = new byte[44];
+        long totalDataLen = (long) dataSize + 36;
+        long bitrate = (long) sampleRate * channel * format;
+        header[0] = 'R';
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f';
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = (byte) format;
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1;
+        header[21] = 0;
+        header[22] = (byte) channel;
+        header[23] = 0;
+        header[24] = (byte) (sampleRate & 0xff);
+        header[25] = (byte) ((sampleRate >> 8) & 0xff);
+        header[26] = (byte) ((sampleRate >> 16) & 0xff);
+        header[27] = (byte) ((sampleRate >> 24) & 0xff);
+        header[28] = (byte) ((bitrate / 8) & 0xff);
+        header[29] = (byte) (((bitrate / 8) >> 8) & 0xff);
+        header[30] = (byte) (((bitrate / 8) >> 16) & 0xff);
+        header[31] = (byte) (((bitrate / 8) >> 24) & 0xff);
+        header[32] = (byte) ((channel * format) / 8);
+        header[33] = 0;
+        header[34] = 16;
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (dataSize & 0xff);
+        header[41] = (byte) ((dataSize >> 8) & 0xff);
+        header[42] = (byte) ((dataSize >> 16) & 0xff);
+        header[43] = (byte) ((dataSize >> 24) & 0xff);
+        return header;
+    }
+
+    public static void rawToWav(final File rawFile, final File waveFile, int sampleRate, int channel, int format) throws IOException {
+        try (FileInputStream fis = new FileInputStream(rawFile); FileOutputStream fos = new FileOutputStream(waveFile)) {
+            fos.write(createWavHeader(sampleRate, channel, format, (int) rawFile.length()));
+            transferTo(fis, fos);
+        }
+    }
+
+    private static long transferTo(InputStream in, OutputStream out) throws IOException {
+        Objects.requireNonNull(in);
+        Objects.requireNonNull(out);
+        long transferred = 0;
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+        int read;
+        while ((read = in.read(buffer, 0, DEFAULT_BUFFER_SIZE)) >= 0) {
+            out.write(buffer, 0, read);
+            transferred += read;
+        }
+        return transferred;
+    }
+}
