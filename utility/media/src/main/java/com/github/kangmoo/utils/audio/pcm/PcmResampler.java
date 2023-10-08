eadf2afd25003f3f4d6acedd7ff315193ec170f1
KangMoo <hkm0629@naver.com>
2023-10-08 16:00:30 +0900

    PCM Re-sampler refactoring

--- utility/media/src/main/java/com/github/kangmoo/utils/audio/pcm/PcmResampler.java
+++ utility/media/src/main/java/com/github/kangmoo/utils/audio/pcm/PcmResampler.java
@@ -1,50 +1,53 @@
 package com.github.kangmoo.utils.audio.pcm;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public class PcmResampler {
-    int inSampleRate;                   // 입력 샘플 레이트
-    int outSampleRate;                  // 출력 샘플 레이트
-    short numChannels;                  // 채널 수
-    short bitsPerSample;                // 샘플당 비트 수
-    private byte[] lastFrameProcessed;  // 마지막으로 처리된 프레임
-    private int decimationOffset;       // 감소 오프셋
+
+    private final int inSampleRate;
+    private final int outSampleRate;
+    private final short numChannels;
+    private final short bitsPerSample;
+    private final int frameSize;
+    private final int padding;
+    private byte[] lastFrameProcessed;
+    private int decimationOffset;
 
     public PcmResampler(int inSampleRate, int outSampleRate) {
         this(inSampleRate, outSampleRate, (short) 1, (short) 16);
     }
 
     private PcmResampler(int inSampleRate, int outSampleRate, short numChannels, short bitsPerSample) {
-        // 입력 값 검증
         if (bitsPerSample <= 0 || bitsPerSample > 16) {
             throw new IllegalArgumentException("The value of bitsPerSample is incorrect.");
-        }
-        if (outSampleRate <= 0) {
-            throw new IllegalArgumentException("The value of outSampleRate is incorrect.");
         }
         this.inSampleRate = inSampleRate;
         this.outSampleRate = outSampleRate;
         this.numChannels = numChannels;
         this.bitsPerSample = bitsPerSample;
+        this.frameSize = (this.bitsPerSample / 8) * this.numChannels;
+        this.padding = ((leastCommonMultiple(inSampleRate, outSampleRate) / inSampleRate) - 1) * frameSize;
     }
 
     public byte[] process(byte[] input) {
-        // 샘플레이트가 같거나 입력 데이터가 없는 경우 처리 필요 없으므로 바로 반환
-        if (outSampleRate == inSampleRate || input.length == 0) {
+        if (noProcessingRequired(input)) {
             return input;
         }
 
-        // 각 프레임의 바이트 크기 및 샘플레이트의 최소공배수 계산
-        int frameSize = (this.bitsPerSample / 8) * this.numChannels;
-        int sampleRateLCM = leastCommonMultiple(inSampleRate, outSampleRate);
+        byte[] stuffedInput = addPaddingToInput(input);
+        performLinearInterpolation(stuffedInput);
+        return performDecimation(stuffedInput);
+    }
 
-        // 패딩을 위한 바이트 수 계산
-        int padding = ((sampleRateLCM / inSampleRate) - 1) * frameSize;
+    private boolean noProcessingRequired(byte[] input) {
+        return outSampleRate == inSampleRate || input.length == 0;
+    }
 
-        // 패딩된 데이터를 위한 배열 생성
-        // 이전에 처리된 프레임이 있으면 그 프레임을 기반으로 패딩
+    private byte[] addPaddingToInput(byte[] input) {
         int baseLength = input.length + (input.length / frameSize * padding);
         byte[] stuffedInput = new byte[lastFrameProcessed == null ? baseLength - padding : baseLength];
+
         int stuffedInputPointer = lastFrameProcessed == null ? 0 : padding;
 
         // 입력 데이터에 패딩 추가
@@ -59,69 +62,87 @@ public class PcmResampler {
             stuffedInputPointer += padding;
         }
 
-        // 선형 보간을 통해 패딩된 데이터의 값을 결정
+        return stuffedInput;
+    }
+
+    private void performLinearInterpolation(byte[] stuffedInput) {
         for (int i = -(lastFrameProcessed == null ? 0 : frameSize); i < stuffedInput.length; i += frameSize + padding) {
-            int j = i + frameSize + padding;
+            processFrameForInterpolation(stuffedInput, i, frameSize, padding);
+        }
+    }
 
-            // 보간할 두 프레임 추출
-            byte[] leftFrame = i >= 0 ? Arrays.copyOfRange(stuffedInput, i, i + frameSize) : lastFrameProcessed;
-            byte[] rightFrame = j >= stuffedInput.length ? null : Arrays.copyOfRange(stuffedInput, j, j + frameSize);
+    private void processFrameForInterpolation(byte[] stuffedInput, int i, int frameSize, int padding) {
+        int j = i + frameSize + padding;
 
-            for (int channel = 0; channel < this.numChannels; channel++) {
-                int leftSample = this.bitsPerSample > 8 ? getIntFromBytes(Arrays.copyOfRange(leftFrame, channel * (this.bitsPerSample / 8), (channel + 1) * (this.bitsPerSample / 8))) : getUnsignedIntFromByte(leftFrame[channel]);
-                int rightSample;
+        byte[] leftFrame = extractFrame(stuffedInput, i, frameSize).orElse(null);
+        byte[] rightFrame = extractFrame(stuffedInput, j, frameSize).orElse(null);
 
-                // 오른쪽 프레임이 null이면 0을 사용
-                if (rightFrame == null) {
-                    rightSample = (byte) 0;
-                } else {
-                    if (this.bitsPerSample > 8) {
-                        int start = channel * (this.bitsPerSample / 8);
-                        int end = (channel + 1) * (this.bitsPerSample / 8);
-                        byte[] subArray = new byte[end - start];
+        for (int channel = 0; channel < this.numChannels; channel++) {
+            int[] samples = extractSamples(leftFrame, rightFrame, channel);
 
-                        System.arraycopy(rightFrame, start, subArray, 0, subArray.length);
+            interpolateSamples(stuffedInput, i, samples, channel, frameSize, padding);
+        }
+    }
 
-                        rightSample = getIntFromBytes(subArray);
-                    } else {
-                        rightSample = getUnsignedIntFromByte(rightFrame[channel]);
-                    }
-                }
+    private Optional<byte[]> extractFrame(byte[] stuffedInput, int index, int frameSize) {
+        if (index >= 0 && index + frameSize <= stuffedInput.length) {
+            return Optional.of(Arrays.copyOfRange(stuffedInput, index, index + frameSize));
+        } else if (index < 0) {
+            return Optional.of(lastFrameProcessed);
+        }
+        return Optional.empty();
+    }
 
-                // 선형 보간 수행
-                for (int k = 0; k < (padding / frameSize); k++) {
-                    int index = i + (frameSize * (k + 1)) + (channel * (this.bitsPerSample / 8));
+    private int[] extractSamples(byte[] leftFrame, byte[] rightFrame, int channel) {
+        int leftSample = (leftFrame != null) ? extractSample(leftFrame, channel) : 0;
+        int rightSample = (rightFrame != null) ? extractSample(rightFrame, channel) : 0;
 
-                    if (index + (this.bitsPerSample / 8) <= stuffedInput.length) {
-                        int interpolatedSample = interpolateLinear(leftSample, rightSample,
-                                (k + 1) / (((double) padding / frameSize) + 1));
+        return new int[]{leftSample, rightSample};
+    }
 
-                        byte[] interpolatedBytes;
+    private int extractSample(byte[] frame, int channel) {
+        if (this.bitsPerSample > 8) {
+            return getIntFromBytes(Arrays.copyOfRange(frame, channel * (this.bitsPerSample / 8), (channel + 1) * (this.bitsPerSample / 8)));
+        }
+        return getUnsignedIntFromByte(frame[channel]);
+    }
 
-                        if (this.bitsPerSample == 8) {
-                            interpolatedBytes = new byte[]{(byte) interpolatedSample};
-                        } else if (this.bitsPerSample == 16) {
-                            interpolatedBytes = getShortBytes((short) interpolatedSample);
-                        } else {
-                            interpolatedBytes = getIntBytes(interpolatedSample);
-                        }
-
-                        System.arraycopy(interpolatedBytes, 0, stuffedInput, index, interpolatedBytes.length);
-                    }
-                }
+    private void interpolateSamples(byte[] stuffedInput, int i, int[] samples, int channel, int frameSize, int padding) {
+        for (int k = 0; k < (padding / frameSize); k++) {
+            int index = i + (frameSize * (k + 1)) + (channel * (this.bitsPerSample / 8));
+            if (index + (this.bitsPerSample / 8) <= stuffedInput.length) {
+                byte[] interpolatedBytes = getInterpolatedBytes(samples, k, padding, frameSize);
+                System.arraycopy(interpolatedBytes, 0, stuffedInput, index, interpolatedBytes.length);
             }
         }
+    }
 
-        // 샘플 감소 (Decimation)
+    private byte[] getInterpolatedBytes(int[] samples, int k, int padding, int frameSize) {
+        int interpolatedSample = interpolateLinear(samples[0], samples[1], (k + 1) / (((double) padding / frameSize) + 1));
+        if (this.bitsPerSample == 8) {
+            return new byte[]{(byte) interpolatedSample};
+        } else if (this.bitsPerSample == 16) {
+            return getShortBytes((short) interpolatedSample);
+        }
+        return getIntBytes(interpolatedSample);
+    }
+
+
+    private byte[] performDecimation(byte[] stuffedInput) {
+        if (inSampleRate <= 0 || outSampleRate <= 0) {
+            throw new IllegalArgumentException("Sampling rate must be positive integer");
+        }
+        int sampleRateLCM = leastCommonMultiple(inSampleRate, outSampleRate);
         int decimationRate = sampleRateLCM / outSampleRate;
+
+        byte[] reSampledInput = new byte[stuffedInput.length];
+
         int outputSize = (int) Math.round(((double) stuffedInput.length / decimationRate));
 
         // 출력 파일의 끝이 일정하지 않으면 추가 바이트 삽입
         while (outputSize % frameSize != 0) {
             outputSize++;
         }
-
-        byte[] reSampledInput = new byte[outputSize];
         int decimatedInputPointer = 0;
 
         // 샘플 감소를 통한 재샘플링
@@ -143,7 +164,7 @@ public class PcmResampler {
         }
 
         // 처리된 마지막 프레임을 저장하여 다음 호출에 사용
-        lastFrameProcessed = Arrays.copyOfRange(input, input.length - frameSize, input.length);
+        lastFrameProcessed = Arrays.copyOfRange(stuffedInput, stuffedInput.length - frameSize, stuffedInput.length);
 
         return reSampledInput;
     }
@@ -158,7 +179,6 @@ public class PcmResampler {
             b = a % b;
             a = temp;
         }
-
         return a;
     }
 
@@ -168,7 +188,6 @@ public class PcmResampler {
         } else if (distance >= 1) {
             return b;
         }
-
         return (int) Math.round(a + ((b - a) * distance));
     }
 
@@ -176,14 +195,18 @@ public class PcmResampler {
         if (data == null || data.length == 0) {
             return 0;
         }
-
-        return switch (data.length) {
-            case 1 -> data[0];
-            case 2 -> (data[1] << 8) | (data[0] & 0xff);
-            case 3 -> (data[2] << 16) | ((data[1] & 0xff) << 8) | (data[0] & 0xff);
-            case 4 -> (data[3] << 24) | ((data[2] & 0xff) << 16) | ((data[1] & 0xff) << 8) | (data[0] & 0xff);
-            default -> throw new IllegalArgumentException();
-        };
+        switch (data.length) {
+            case 1:
+                return data[0];
+            case 2:
+                return (data[1] << 8) | (data[0] & 0xff);
+            case 3:
+                return (data[2] << 16) | ((data[1] & 0xff) << 8) | (data[0] & 0xff);
+            case 4:
+                return (data[3] << 24) | ((data[2] & 0xff) << 16) | ((data[1] & 0xff) << 8) | (data[0] & 0xff);
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
     private static int getUnsignedIntFromByte(byte data) {
