dee958555ce18bd697579b77fe40fd6f3846f357
KangMoo <hkm0629@naver.com>
2024-05-24 16:05:48 +0900

    update

--- utility/common/src/main/java/com/github/kangmoo/utils/common/FileLock.java
+++ utility/common/src/main/java/com/github/kangmoo/utils/common/FileLock.java
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 @Slf4j
 public class FileLock {
 
-    private static void processLock(String name) {
+    public static void processLock(String name) {
         Path lockFilePath = Path.of(System.getProperty("java.io.tmpdir")).resolve(name + ".lock");
 
         try {
